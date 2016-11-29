d21c2685f01a85574d55fbc407bfe15b1b626163
Jón Levy <nonni@nonni.cc>
2016-11-29 13:28:53 +0000

    simplify imports of external text data

--- utils/additional_data_extractor.py
+++ utils/additional_data_extractor.py
@@ -4,38 +4,68 @@ import sys
 
 cwd = os.path.dirname(__file__)
 
+def run_txt_parser(file):
+    """ Read input file and output CSV """
+    out = codecs.open(file['outfile'], 'a', 'utf-8')
+    with codecs.open(file['infile'], 'r', 'iso-8859-1') as inp:
+        for li in inp:            
+            if li:
+                li = li.strip()
+            if li:
+                if not li.startswith('#'):
+                    forms = li.split(u' ')
+                    if forms:                        
+                        word = forms[0]
+                        for f in forms[1:]:
+                            s = u'{0};{1};{2};{3};{4};{5}\n'.format(
+                                word, 0, f, u'ob', word, u'-'
+                            )                                                
+                            out.write(s)
+
+def run_csv_parser(file):
+
+    out = codecs.open(file['outfile'], 'a', file['out-encoding'])
+
+    with codecs.open(file['infile'], 'r', file['in-encoding']) as f:
+        lines = f.readlines()
+        for i, line in enumerate(lines):
+            if line.split() and i >= file['start-line']:                
+                out.write(line)
+
 files = [
     {
         'infile': cwd+'../resources/plastur.feb2013.txt',
         'outfile': cwd+'../resources/ord.csv',
         'in-encoding': 'iso-8859-1',
         'out-encoding': 'utf-8',
-        'start-line': 33
+        'start-line': 33,
+        'func': run_csv_parser
     },
     {
         'infile': cwd + '../resources/SHsnid.csv',
         'outfile': cwd+'../resources/ord.csv',
         'in-encoding': 'utf-8',
         'out-encoding': 'utf-8',
-        'start-line': 0
+        'start-line': 0,
+        'func': run_csv_parser
     },    
+    {
+        'infile': cwd + '../resources/obeyg.smaord.txt',
+        'outfile': cwd+'../resources/ord.csv',
+        'in-encoding': 'iso-8859-1',
+        'out-encoding': 'utf-8',
+        'start-line': 38,
+        'func': run_txt_parser
+    },     
 ]
 
-def run():
-
-    for file in files:
-
-        out = codecs.open(file['outfile'], 'a', file['out-encoding'])
-
-        with codecs.open(file['infile'], 'r', file['in-encoding']) as f:
-            lines = f.readlines()
-            for i, line in enumerate(lines):
-                if line.split() and i >= file['start-line']:
-                    #print(i, line)
-                    out.write(line)
 
 if __name__ == '__main__':
     for file in files:
         if not os.path.isfile(file['infile']):            
-            sys.exit('Neccessary file is missing: {}'.format(file['infile']))            
-    run()
+            sys.exit('Neccessary file is missing: {}'.format(file['infile']))
+        else:
+            print('Retrieving data from file: {}'.format(file['infile']))
+            file['func'](file)
+            print('Data appended successfully to file: {}'.format(file['outfile']))
+    print('And we are done :D')
