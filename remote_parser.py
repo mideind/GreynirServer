58d933e7bf61c8dec429c9eaa71301f067cd08b8
haukurb <haukzi@gmail.com>
2018-08-31 15:44:36 +0000

    Fixed connection issue and json decoding error

--- remote_parser.py
+++ remote_parser.py
@@ -28,7 +28,7 @@ import socket
 import json
 
 _PORT = 9000
-_BUFFER_SIZE = 2 ** 13  # 8192
+_BUFFER_SIZE = 2 ** 15  # 32768
 _TIMEOUT_REQUEST = 10
 _HOST = "localhost"
 
@@ -45,6 +45,7 @@ class RemoteParser:
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def __enter__(self):
+        print("Trying to connect to {}:{}".format(self.host, self.port))
         self.sock.connect((self.host, self.port))
         return self
 
@@ -53,31 +54,23 @@ class RemoteParser:
         self.sock.close()
         return False
 
-    def parse(self, line, flat=True, incomplete=True):
-        msg = line.split("\n")[0] # only process first sentence
-        encoded_msg = bytearray([1 if flat else 0, 1 if incomplete else 0])
-        encoded_msg.extend(msg.encode("utf-8"))
-        self.sock.send(encoded_msg)
+    def parse(self, text, version=1):
+        lines = text.split("\n")
+        obj = dict(
+            version=version,
+            sentences=lines,
+        )
+        payload = json.dumps(obj)
+        self.sock.send(payload.encode("utf-8"))
         data = self.sock.recv(self.buffer_size)
         try:
-            tree_dict = json.loads(data.decode("utf-8"))
+            resp = json.loads(data.decode("utf-8"))
         except Exception:
-            print("Error parsing text: {text}".format(text=msg))
-            print("Received: {data}".format(data=data))
+            print("Error parsing text from nnserver: {data}".format(data=data))
+            print("Received: {text}".format(text=text))
             return None
-        return tree_dict
+        return resp
 
-
-def remote_parse_test():
-    with RemoteParser() as parser:
-        tree = parser.parse("Ísland keppti í HM fyrir stuttu.")
-        print(tree)
-        return tree
-
-
-def t1():
-    with RemoteParser() as parser:
-        text = "Snúðurinn sem fólkið keypti var ekki góður."
-        sent = text.split("\n")
-        tree_dict = parser.parse(sent)
-        print(json.dumps(tree_dict, indent=4, ensure_ascii=False))
+    def parse_sentence(self, line):
+        single_line = line.split("\n")[:1]
+        return self.parse(line)
