87f76c2009967e2d47f4c4a0ab7a4f22f57a7c90
haukurb <haukzi@gmail.com>
2018-09-06 19:13:33 +0000

    Removed unnecessary files and fixed dates

--- composite_encoder.py
+++ composite_encoder.py
@@ -6,7 +6,7 @@
 
     Neural Network Parsing Encoder
 
-    Copyright (C) 2017 Vilhjálmur Þorsteinsson
+    Copyright (C) 2018 Vilhjálmur Þorsteinsson
 
        This program is free software: you can redistribute it and/or modify
        it under the terms of the GNU General Public License as published by
