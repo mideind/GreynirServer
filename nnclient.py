a1c2ba05b70f406770df3ee56e086e038c05f489
haukurb <haukzi@gmail.com>
2018-09-06 19:04:42 +0000

    Neural network server up and running

--- nnclient.py
+++ nnclient.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 """
     Reynir: Natural language processing for Icelandic
 
     Neural Network Query Client
 
-    Copyright (C) 2018 Miðeind
+    Copyright (C) 2018 Vilhjálmur Þorsteinsson
 
        This program is free software: you can redistribute it and/or modify
        it under the terms of the GNU General Public License as published by
@@ -24,22 +25,40 @@
 
 """
 
-import json
+import tokenizer
 import requests
+import base64
+import json
 
 from settings import Settings
-import tokenizer
 
+from composite_encoder import CompositeTokenEncoder as ParsingEncoder
 import nntree
 from nntree import ParseResult
+import grammar_consts as gu
+
+from tensor2tensor.data_generators import text_encoder
+from tensorflow.core.example import feature_pb2
+from tensorflow.core.example import example_pb2
+from google.protobuf import text_format
+
+EOS_ID = text_encoder.EOS_ID
+PAD_ID = text_encoder.PAD_ID
+_PARSING_VOCAB_PATH = "parsing_tokens_180729.txt"
+_ENIS_VOCAB_PATH = "vocab.enis.16384.subwords"
 
 
 class NnClient:
     """ Client that speaks to the HTTP RESTful interface of
         a tensorflow model server. """
 
-    port = Settings.NN_PORT
-    host = Settings.NN_HOST
+    port=Settings.NN_PORT
+    host=Settings.NN_HOST
+    _tfms_version="v1"
+    _model_name="transformer"
+    _verb="predict"
+    _parsingEncoder = ParsingEncoder(_PARSING_VOCAB_PATH, version=2)
+    _enisEncoder = text_encoder.SubwordTextEncoder(_ENIS_VOCAB_PATH)
 
     @classmethod
     def parse_sentence(cls, text):
@@ -60,17 +79,20 @@ class NnClient:
     @classmethod
     def _request(cls, pgs):
         """ Send serialized request to remote model server """
-        url = "http://{host}:{port}/parse.api".format(host=cls.host, port=cls.port)
+        url = "http://{host}:{port}/{version}/models/{model}:{verb}".format(
+            port=cls.port,
+            host=cls.host,
+            version=cls._tfms_version,
+            model=cls._model_name,
+            verb=cls._verb
+        )
         headers = {"content-type": "application/json"}
+        instances = [cls._serializeToInstance(sent) for sent in pgs]
 
-        normalized_pgs = [
-            [tok.txt for tok in list(tokenizer.tokenize(pg)) if tok] for pg in pgs
-        ]
-        normalized_pgs = [" ".join([tok for tok in npg if tok]) for npg in normalized_pgs]
         payload = {
-            "pgs": normalized_pgs,
+            "signature_name" : "serving_default",
+            "instances" : [inst for inst in instances],
         }
-
         payload = json.dumps(payload)
         resp = requests.post(url, data=payload, headers=headers)
 
@@ -112,10 +134,37 @@ class NnClient:
             print("Output: {parse_toks}".format(parse_toks=parse_toks))
         return tree
 
+    @classmethod
+    def _serializeToInstance(cls, sent):
+        """ Encodes a single sentence into the format expected by the RESTful interface
+        of tensorflow_model_server running an exported tensor2tensor transformer translation model """
+        # Add end of sentence token
+        input_ids = cls._enisEncoder.encode(sent)
+        input_ids.extend([EOS_ID])
+
+        int64_list = feature_pb2.Int64List(value=input_ids)
+        feature = feature_pb2.Feature(int64_list=int64_list)
+        feature_map = {
+            "inputs" : feature
+        }
+        features = feature_pb2.Features(feature=feature_map)
+        example = example_pb2.Example(features=features)
+
+        b64_example = base64.b64encode(example.SerializeToString()).decode("utf-8")
+        return {
+            "input" : {
+                "b64" : b64_example
+            }
+        }
+
 
 def test_sentence():
     res = NnClient.parse_sentence("Eftirfarandi skilaboð voru smíðuð í NNCLIENT.")
     print("Received response:")
+
+
+def manual_test():
+    res = NnClient.parse_sentence("Konurnar komu heim.")
     print(res)
 
 
