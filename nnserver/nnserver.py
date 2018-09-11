fa43442b39694b6c081befd56f2fa02e291f4b50
haukurb <haukzi@gmail.com>
2018-09-11 12:48:54 +0000

    NN Server now works
    Also moved composite encoder to nnserver

--- nnserver/nnserver.py
+++ nnserver/nnserver.py
@@ -21,7 +21,8 @@
     along with this program.  If not, see http://www.gnu.org/licenses/.
 
 
-    This module implements a client for that connects to a tensorflow model server.
+    This module implements a server that provides an icelandic text interface to
+    an NMT parsing tensorflow model server.
 
 """
 
@@ -29,31 +30,33 @@ import tokenizer
 import requests
 import base64
 import json
-
-from settings import Settings
+import requests
 
 from composite_encoder import CompositeTokenEncoder as ParsingEncoder
-import nntree
-from nntree import ParseResult
-import grammar_consts as gu
 
 from tensor2tensor.data_generators import text_encoder
 from tensorflow.core.example import feature_pb2
 from tensorflow.core.example import example_pb2
-from google.protobuf import text_format
+
+from flask import Flask, jsonify, request
+
 
 EOS_ID = text_encoder.EOS_ID
 PAD_ID = text_encoder.PAD_ID
-_PARSING_VOCAB_PATH = "parsing_tokens_180729.txt"
-_ENIS_VOCAB_PATH = "vocab.enis.16384.subwords"
+
+_PARSING_VOCAB_PATH = "../resources/parsing_tokens_180729.txt"
+_ENIS_VOCAB_PATH = "../resources/vocab.enis.16384.subwords"
 
 
-class NnClient:
-    """ Client that speaks to the HTTP RESTful interface of
-        a tensorflow model server. """
+app = Flask(__name__)
 
-    port = Settings.NN_PORT
-    host = Settings.NN_HOST
+
+class NnServer:
+    """ Client that minics the HTTP RESTful interface of
+        a tensorflow model server, but accepts plain text. """
+
+    host = app.config.get("host")
+    port = app.config.get("port")
     _tfms_version = "v1"
     _model_name = "transformer"
     _verb = "predict"
@@ -61,23 +64,7 @@ class NnClient:
     _enisEncoder = text_encoder.SubwordTextEncoder(_ENIS_VOCAB_PATH)
 
     @classmethod
-    def parse_sentence(cls, text):
-        """ Parse a single sentence into flat parse tree """
-        if "\n" in text:
-            single_sentence = text.split("\n")[0]
-        else:
-            single_sentence = text
-            pgs = [single_sentence]
-        return cls._request(pgs)[0]
-
-    @classmethod
-    def parse_text(cls, text):
-        """ Parse contiguous text into flat parse trees """
-        pgs = text.split("\n")
-        return cls._request(pgs)
-
-    @classmethod
-    def _request(cls, pgs):
+    def request(cls, pgs):
         """ Send serialized request to remote model server """
         url = "http://{host}:{port}/{version}/models/{model}:{verb}".format(
             port=cls.port,
@@ -87,7 +74,7 @@ class NnClient:
             verb=cls._verb,
         )
         headers = {"content-type": "application/json"}
-        instances = [cls._serializeToInstance(sent) for sent in pgs]
+        instances = [cls.serialize_to_instance(sent) for sent in pgs]
 
         payload = {
             "signature_name": "serving_default",
@@ -100,43 +87,36 @@ class NnClient:
             obj = json.loads(resp.text)
             predictions = obj["predictions"]
             results = [
-                cls._processResponse(inst, sent)
+                cls.process_response_instance(inst, sent)
                 for (inst, sent) in zip(predictions, pgs)
             ]
-            score_str = " ".join(
-                ["{:>4.2f}".format(max(inst["scores"])) for inst in predictions]
-            )
-
-            print(
-                "Parsed {num} sentences with neural network with scores: {scores}".format(
-                    num=len(predictions), scores=score_str
-                )
-            )
-            return results
-        # TODO(haukurb): More graceful error handlign
+            # return results
+            obj["predictions"] = results
+            return obj
         except Exception as e:
             print("Error: could not process response from nnserver.")
             print(e)
             return None
 
     @classmethod
-    def _processResponse(cls, instance, sent):
-        """ Process the response from a single sentence """
-        parse_toks = instance["outputs"]
-        print(parse_toks)
-        tree, p_result = nntree.parse_tree_with_text(parse_toks, sent)
-        tree.pprint()
-        tree = tree.to_dict()
-        print(tree)
+    def process_response_instance(cls, instance, sent):
+        """  Process the numerical output from the model server for one sentence """
+        scores = instance["scores"]
+        output_ids = instance["outputs"]
 
-        if p_result != ParseResult.SUCCESS:
-            print("NnParse not successful for input: '{text}'".format(text=sent))
-            print("ParseResult: {result}".format(result=p_result))
-            print("Output: {parse_toks}".format(parse_toks=parse_toks))
-        return tree
+        # Strip padding and eos token
+        length = len(output_ids)
+        pad_start = output_ids.index(PAD_ID) if PAD_ID in output_ids else length
+        eos_start = output_ids.index(EOS_ID) if EOS_ID in output_ids else length
+        sent_end = min(pad_start, eos_start)
+        output_toks = cls._parsingEncoder.decode(output_ids[:sent_end])
+        instance["outputs"] = output_toks
+
+        # return output_toks
+        return instance
 
     @classmethod
-    def _serializeToInstance(cls, sent):
+    def serialize_to_instance(cls, sent):
         """ Encodes a single sentence into the format expected by the RESTful interface
         of tensorflow_model_server running an exported tensor2tensor transformer translation model """
         # Add end of sentence token
@@ -153,15 +133,64 @@ class NnClient:
         return {"input": {"b64": b64_example}}
 
 
-def test_sentence():
-    res = NnClient.parse_sentence("Eftirfarandi skilaboð voru smíðuð í NNCLIENT.")
-    print("Received response:")
-
-
-def manual_test():
-    res = NnClient.parse_sentence("Konurnar komu heim.")
-    print(res)
+@app.route("/parse.api", methods=["POST"])
+def test():
+    try:
+        req_body = request.data.decode("utf-8")
+        obj = json.loads(req_body)
+        # TODO: validate form?
+        pgs = obj["pgs"]
+        model_response = NnServer.request(pgs)
+        resp = jsonify(model_response)
+        resp.headers["Content-Type"] = "application/json; charset=utf-8"
+        return resp
+    except:
+        resp = jsonify(valid=False, reason="Invalid request")
+        resp.headers["Content-Type"] = "application/json; charset=utf-8"
+        return resp
 
 
 if __name__ == "__main__":
-    manual_test()
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description=(
+            "Middleware server for providing a textual interface to tensorflow model server"
+        )
+    )
+    parser.add_argument(
+        "--listen_host",
+        dest="IN_HOST",
+        required=False,
+        type=str,
+        default="localhost",
+        help="Hostname to listen on",
+    )
+    parser.add_argument(
+        "--listen_port",
+        dest="IN_PORT",
+        required=False,
+        type=str,
+        default="8080"
+        help="Port to listen on",
+    )
+    parser.add_argument(
+        "--model_host",
+        dest="OUT_HOST",
+        required=False,
+        type=str,
+        default="localhost",
+        help="Hostname of model server",
+    )
+    parser.add_argument(
+        "--model_port",
+        dest="9000",
+        required=False,
+        type=str,
+        help="Port of model server",
+    )
+
+    args = parser.parse_args()
+    app.config["out_host"] = args.OUT_HOST
+    app.config["out_port"] = args.OUT_PORT
+    app.run(debug=True, host=args.IN_HOST, port=args.IN_PORT)
