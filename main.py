ec14307888b4bef6c1d654363a88b6619aa88b70
Hulda Óladóttir <holado@gmail.com>
2018-09-22 15:48:08 +0000

    Adjustments to ReynirCorrect started

--- main.py
+++ main.py
@@ -48,9 +48,10 @@ from flask.wrappers import Response
 import reynir
 from settings import Settings, ConfigError, changedlocale
 from reynir.bindb import BIN_Db
-from nertokenizer import tokenize_and_recognize, correct_spaces
+from nertokenizer import recognize_entities, correct_spaces
 from reynir.binparser import canonicalize_token
 from reynir.fastparser import Fast_Parser, ParseForestFlattener
+from reynir_correct import tokenize
 from article import Article as ArticleProxy
 from treeutil import TreeUtility
 from scraperdb import (
@@ -678,12 +679,12 @@ def query_api(version=1):
             result["response"] = _SPECIAL_QUERIES[ql + "?"]
     else:
         with SessionContext(commit=True) as session:
-
+            a_u = q.islower() if auto_uppercase else False
+            tokstream = tokenize(text, a_u)
             toklist = list(
-                tokenize_and_recognize(
+                recognize_entities(
                     q,
                     enclosing_session=session,
-                    auto_uppercase=q.islower() if auto_uppercase else False,
                 )
             )
             actual_q = correct_spaces(" ".join(t.txt or "" for t in toklist))
