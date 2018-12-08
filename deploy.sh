af9a0419e603c4eae3fceff3949bfafbab179c5e
Vilhjalmur Thorsteinsson <vt@extrada.com>
2018-12-08 17:20:17 +0000

    Deleted Main.conf and references to incparser.py

--- deploy.sh
+++ deploy.sh
@@ -27,14 +27,16 @@ echo "Copying files"
 
 cp config/Adjectives.conf $DEST/config/Adjectives.conf
 cp config/Index.conf $DEST/config/Index.conf
-cp config/Main.conf $DEST/config/Main.conf
+# Main.conf is no longer needed
+rm $DEST/config/Main.conf
 # Note: config/Reynir.conf is not copied
 cp config/TnT-model.pickle $DEST/config/TnT-model.pickle
 
 cp article.py $DEST/article.py
 cp fetcher.py $DEST/fetcher.py
 cp getimage.py $DEST/getimage.py
-#cp incparser.py $DEST/incparser.py
+# incparser.py is no longer needed
+rm $DEST/incparser.py
 cp main.py $DEST/main.py
 cp nertokenizer.py $DEST/nertokenizer.py
 cp postagger.py $DEST/postagger.py
