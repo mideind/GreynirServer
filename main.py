2c544c15c0988910b142a95776027bb6988ddd59
Vilhjalmur Thorsteinsson <vt@extrada.com>
2019-02-04 01:32:24 +0000

    Vocabulary additions; style fixes

--- main.py
+++ main.py
@@ -5,7 +5,8 @@
 
     Web server main module
 
-    Copyright (C) 2018 Miðeind ehf.
+    Copyright (C) 2019 Miðeind ehf.
+    Original author: Vilhjálmur Þorsteinsson
 
        This program is free software: you can redistribute it and/or modify
        it under the terms of the GNU General Public License as published by
