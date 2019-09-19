2897f2135e345c8556a412c981c0ff9eca329ad3
Sveinbjorn Thordarson <sveinbjorn@sveinbjorn.org>
2019-09-19 17:05:21 +0000

    Renamed frivolous.py to special.py, added random joke query support + responses to simple queries concerning capabilities.

--- queries/special.py
+++ queries/special.py
@@ -28,92 +28,124 @@
 """
 
 from datetime import datetime, timedelta
+from inspect import isfunction
+from random import choice
+
 
 _SPECIAL_QTYPE = "Special"
 
+
+_JOKES = (
+    "Af hverju taka Hafnfirðingar alltaf stiga út í búð? Því verðið er svo hátt.",
+    "Af hverju búa Hafnfirðingar í kringlóttum húsum? Svo enginn mígi í hornin.",
+    "Af hverju eru Hafnfirðingar alltaf með stól úti á svölum? Svo sólin geti sest.",
+    "Af hverju læðast Hafnfirðingar alltaf fram hjá apótekum? Til að vekja ekki svefnpillurnar.",
+    "Af hverju fara Hafnfirðingar alltaf niður í fjöru um jólin? Til þess að bíða eftir jólabókaflóðinu.",
+)
+
+
+def _random_joke():
+    return { "answer": choice(_JOKES) }
+
+
+def _capabilities():
+    return { "answer": "Þú getur meðal annars spurt mig um veðrið." }
+
+
 _SPECIAL_QUERIES = {
-    "er þetta spurning?": {
+    "er þetta spurning": {
         "answer": "Er þetta svar?"
     },
-    "er þetta svar?": {
+    "er þetta svar": {
         "answer": "Er þetta spurning?"
     },
-    "hvað er svarið?": {
+    "hvað er svarið": {
         "answer": "42.",
         "voice": "Fjörutíu og tveir."
     },
-    "hvert er svarið?": {
+    "hvert er svarið": {
         "answer": "42.",
         "voice": "Fjörutíu og tveir."
     },
-    "veistu allt?": {
+    "veistu allt": {
         "answer": "Nei, því miður."
     },
-    "hvað veistu?": {
+    "hvað veistu": {
         "answer": "Spurðu mig!"
     },
-    "veistu svarið?": {
+    "veistu svarið": {
         "answer": "Spurðu mig!"
     },
-    "hvað heitir þú?": {
+    "hvað heitir þú": {
         "answer": "Greynir. Ég er grey sem reynir að greina íslensku.",
         "voice": "Ég heiti Greynir. Ég er grey sem reynir að greina íslensku."
     },
-    "hver ert þú?": {
+    "hver ert þú": {
         "answer": "Ég er grey sem reynir að greina íslensku."
     },
-    "hver bjó þig til?": {
+    "hver bjó þig til": {
         "answer": "Flotta teymið hjá Miðeind.",
     },
-    "hver skapaði þig?": {
+    "hver skapaði þig": {
         "answer": "Flotta teymið hjá Miðeind."
     },
-    "hver er skapari þinn?": {
+    "hver er skapari þinn": {
         "answer": "Flotta teymið hjá Miðeind."
     },
-    "hver er flottastur?": {
+    "hver er flottastur": {
         "answer": "Teymið hjá Miðeind."
     },
-    "hver er sætastur?": {
+    "hver er sætastur": {
         "answer": "Tumi Þorsteinsson.",
         "voice": "Tumi Þorsteinsson er langsætastur."
     },
-    "hver er langsætastur?": {
+    "hver er langsætastur": {
         "answer": "Tumi Þorsteinsson.",
         "voice": "Tumi Þorsteinsson er langsætastur."
     },
-    "hver er lang sætastur?": {
+    "hver er lang sætastur": {
         "answer": "Tumi Þorsteinsson.",
         "voice": "Tumi Þorsteinsson er langsætastur."
     },
-    "hver er ég?": {
+    "hver er ég": {
         "answer": "Þú ert þú."
     },
-    "hvað er ég?": {
+    "hvað er ég": {
         "answer": "Þú ert væntanlega manneskja sem talar íslensku."
     },
-    "er guð til?": {
+    "er guð til": {
         "answer": "Ég held ekki."
     },
-    "hver skapaði guð?": {
+    "hver skapaði guð": {
         "answer": "Enginn sem ég þekki."
     },
-    "hver skapaði heiminn?": {
+    "hver skapaði heiminn": {
         "answer": "Enginn sem ég þekki."
     },
-    "hver er tilgangur lífsins?": {
+    "hver er tilgangur lífsins": {
         "answer": "42.",
         "voice": "Fjörutíu og tveir."
     },
-    "hvar endar alheimurinn?": {
+    "hvar endar alheimurinn": {
         "answer": "Inni í þér."
     },
-    "hvar er draumurinn?": {
+    "hvar er draumurinn": {
         "answer": "Hvar ertu, lífið sem ég þrái?"
     },
-    "hvað get ég spurt þig um?": {
-        "answer": "Þú getur meðal annars spurt mig um veðrið."
+
+    "hvað get ég spurt þig um": _capabilities,
+    "hvað er hægt að spyrja um": _capabilities,
+    "hvað er hægt að spyrja þig um": _capabilities,
+    "hvaða spurningar skilur þú": _capabilities,
+    "hvers konar spurningar skilur þú": _capabilities,
+
+    "ertu með kímnigáfu": {
+        "answer": "Afar takmarkaða.",
+        "voice": "Já, en afar takmarkaða",
     },
+    "segðu mér brandara": _random_joke,
+    "segðu brandara": _random_joke,
+    "komdu með brandara": _random_joke,
 }
 
 
@@ -124,19 +156,26 @@ def handle_plain_text(q):
         the appropriate properties on the Query instance have
         been set, such as the answer and the query type (qtype).
         If the query is not recognized, returns False. """
-    ql = q.query_lower
-    if not ql.endswith("?"):
-        ql += "?"
+    ql = q.query_lower.rstrip('?')
+
     if ql in _SPECIAL_QUERIES:
         # This is a query we recognize and handle
         q.set_qtype(_SPECIAL_QTYPE)
-        response = _SPECIAL_QUERIES[ql]
+        r = _SPECIAL_QUERIES[ql]
+        fixed = not isfunction(r)
+        response = r if fixed else r()
+
         # A non-voice answer is usually a dict or a list
         answer = response.get("answer")
         # A voice answer is always a plain string
         voice = response.get("voice") or answer
         q.set_answer(dict(answer=answer), answer, voice)
-        q.set_expires(datetime.utcnow() + timedelta(minutes=10))
+
+        # Caching
+        now = datetime.utcnow()
+        exp = now + timedelta(minutes=10) if fixed else now
+        q.set_expires(exp)
+
         return True
 
     return False
