0bf7765664d6d88545d353343cac718ba483a189
Sveinbjorn Thordarson <sveinbjorn@sveinbjorn.org>
2020-03-13 21:29:41 +0000

    Fixed reparse script + additions to Vocab.conf

--- scripts/runreparse.sh
+++ scripts/runreparse.sh
@@ -3,7 +3,7 @@
 # This is run once every morning by cron
 #
 cd ~/github/Greynir
-source p3510/bin/activate
+source p369/bin/activate
 timeout 120m python scraper.py --reparse --limit=5000
 timeout 40m python processor.py --update --limit=10000
 deactivate
