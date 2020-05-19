bd25a105cf86c6c591d3c84e856ef00b29b350de
Sveinbjorn Thordarson <sveinbjorn@sveinbjorn.org>
2020-05-19 18:12:26 +0000

    Additions to geo-related data

--- geo.py
+++ geo.py
@@ -131,6 +131,7 @@ COUNTRY_NAME_TO_ISOCODE_ADDITIONS = {
         "Mjanmar": "MM",
         "Myanmar": "MM",
         "Bahrain": "BH",
+        "Bandaríki Norður-Ameríku": "US",
         "Búrma": "MM",
         "Burma": "MM",
         "Ameríka": "US",
@@ -191,8 +192,10 @@ COUNTRY_NAME_TO_ISOCODE_ADDITIONS = {
         "Norður-Noregur": "NO",
         "Tæland": "TH",
         "Aserbaísjan": "AZ",  # Til svona stafsett í BÍN
+        "Aserbædjan": "AZ",
         "Azerbaijan": "AZ",
         "Kanarí": "IC",
+        "Kanaríeyjar": "IC",
         "Jómfrúaeyjar": "US",
         "Ghana": "GH",
         "Kosovo": "XK",
