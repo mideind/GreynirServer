b40225d10e10604de38b8f0a7252cd187a07aee1
Sveinbjorn Thordarson <sveinbjorn@sveinbjorn.org>
2020-07-01 14:29:21 +0000

    Added sample home.py query module

---
+++ queries/home.py
@@ -0,0 +1,38 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def handle_plain_text(q):
+    """ Handle a plain text query, contained in the q parameter
+        which is an instance of the query.Query class.
+        Returns True if the query was handled, and in that case
+        the appropriate properties on the Query instance have
+        been set, such as the answer and the query type (qtype).
+        If the query is not recognized, returns False. """
+    ql = q.query_lower.rstrip('?')
+
+
+    if ql == "kveiktu ljós":
+
+        q.set_qtype("Home")
+
+        # A non-voice answer is usually a dict or a list
+        answer = "Skal gert"
+
+        q.set_answer(dict(answer=answer), answer, answer)
+
+        q.set_command("2+2")
+
+
+    return True
