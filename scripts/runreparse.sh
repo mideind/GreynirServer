472bc97335df7153b420fe1570d25fed53a0f121
Sveinbjorn Thordarson <sveinbjorn@sveinbjorn.org>
2020-10-15 15:07:52 +0000

    Updated scripts to assume venv is the name of virtual environment, not p369 (symlinked in production env)

--- scripts/runreparse.sh
+++ scripts/runreparse.sh
@@ -3,7 +3,7 @@
 # This is run once every morning by cron
 #
 cd ~/Greynir
-source p369/bin/activate
-GREYNIR_DB_HOST="greynir.is" timeout 120m python scraper.py --reparse --limit=5000
+source venv/bin/activate
+GREYNIR_DB_HOST="greynir.is" timeout 120m python scraper.py --reparse --limit=7500
 GREYNIR_DB_HOST="greynir.is" timeout 40m python processor.py --update --limit=10000
 deactivate
