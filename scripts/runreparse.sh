41f3d17582177b1153b66bdbd0a8d25e74236250
Sveinbjorn Thordarson <sveinbjorn@sveinbjorn.org>
2021-03-19 13:28:21 +0000

    Add shellcheck syntax checking to CI workflow

--- scripts/runreparse.sh
+++ scripts/runreparse.sh
@@ -3,6 +3,7 @@
 # This is run once every morning by cron
 #
 cd ~/Greynir || exit 1
+# shellcheck disable=SC1091
 source venv/bin/activate
 GREYNIR_DB_HOST="greynir.is" timeout 120m python scraper.py --reparse --limit=7500
 GREYNIR_DB_HOST="greynir.is" timeout 40m python processor.py --update --limit=10000
--- scripts/runscraper.sh
+++ scripts/runscraper.sh
@@ -2,6 +2,7 @@
 
 # Scraper
 cd ~/Greynir || exit 1
+# shellcheck disable=SC1091
 source venv/bin/activate
 GREYNIR_DB_HOST="greynir.is" timeout 20m python scraper.py --limit=2500
 # Use control group to limit memory usage and swap
@@ -10,6 +11,7 @@ deactivate
 
 # Processor
 cd ~/Greynir || exit 1
+# shellcheck disable=SC1091
 source venv/bin/activate
 GREYNIR_DB_HOST="greynir.is" timeout 20m python processor.py --limit=3000
 deactivate
