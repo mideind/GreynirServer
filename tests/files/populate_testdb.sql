1f348e087e2390710459803c21784fc3d6d6bf9e
Sveinbjorn Thordarson <sveinbjorn@sveinbjorn.org>
2021-05-25 13:15:05 +0000

    Fix CI test

--- tests/files/populate_testdb.sql
+++ tests/files/populate_testdb.sql
@@ -8,7 +8,8 @@ INSERT INTO articles ("url", "id", "root_id", "heading", "author", "timestamp", 
 INSERT INTO persons ("id", "article_url", "name", "title", "title_lc", "gender", "authority", "timestamp") VALUES
 (1, 'https://greynir.is/', 'Björn Þorsteinsson', 'prófessor í heimspeki', 'prófessor í heimspeki', 'kk', 1, '2019-02-11 13:44:41.635725'),
 (2, 'https://greynir.is/', 'Viðar Þorsteinsson', 'framkvæmdastjóri Eflingar', 'framkvæmdastjóri eflingar', 'kk', 1, '2019-02-11 13:44:41.992479'),
-(3, 'https://greynir.is/', 'Katrín Jakobsdóttir', 'forsætisráðherra', 'forsætisráðherra', 'kvk', 1, '2019-02-11 13:44:41.994941');
+(3, 'https://greynir.is/', 'Katrín Jakobsdóttir', 'forsætisráðherra', 'forsætisráðherra', 'kvk', 1, '2019-02-11 13:44:41.994941'),
+(4, 'https://greynir.is/', 'Jón Jónsson', 'forstjóri Sjóvá', 'forstjóri sjóvá', 'kk', 1, '2019-02-11 13:44:41.994941');
 
 INSERT INTO queries ("id", "timestamp", "interpretations", "question", "bquestion", "answer", "voice", "error", "expires", "qtype", "key", "client_type", "client_version", "client_id", "latitude", "longitude", "remote_addr", "context") VALUES
 ('c619a8ec-72c2-11ea-99c0-820d5d3a9700', '2020-03-30 20:12:25.985256', '["hver er forsætisráðherra"]', 'hver er forsætisráðherra', 'Hver er forsætisráðherra?', 'Katrín Jakobsdóttir', 'Forsætisráðherra er Katrín Jakobsdóttir.', NULL, NULL, 'Title', 'forsætisráðherra', NULL, NULL, '123', NULL, NULL, '127.0.0.1', '{"person_name": "Katrín Jakobsdóttir"}'),
