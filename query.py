b1644428fd4b7921f2dc0ad739b67d358f32312a
Logi E <logi.eyjolfsson@gmail.com>
2021-05-26 13:08:32 +0000

    Added HTML sanitization for response

--- query.py
+++ query.py
@@ -52,6 +52,7 @@ from datetime import datetime, timedelta
 import json
 import re
 import random
+from html import escape
 from collections import defaultdict
 
 from settings import Settings
@@ -784,6 +785,8 @@ class Query:
         self, response: ResponseType, answer: str, voice_answer: Optional[str] = None
     ) -> None:
         """ Set the answer to the query """
+        if isinstance(response, dict) and isinstance(response.get("answer"), str):
+            response["answer"] = escape(response["answer"]) # Sanitize answer
         # Detailed response (this is usually a dict)
         self._response = response
         # Single best answer, as a displayable string
