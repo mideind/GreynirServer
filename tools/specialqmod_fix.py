dece31fb60e97a3233dbc4cdf6410811a59fa9f3
Sveinbjorn Thordarson <sveinbjorn@sveinbjorn.org>
2021-09-27 17:58:35 +0000

    Added tool to fix hardcoded queries in special qmodule

---
+++ tools/specialqmod_fix.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+#
+# This program reads all logged queries in the database and dumps
+# various stats wrt. unanswered queries + more.
+
+import os
+import sys
+
+# Hack to make this Python program executable from the tools subdirectory
+basepath, _ = os.path.split(os.path.realpath(__file__))
+_TOOLS = os.sep + "tools"
+if basepath.endswith(_TOOLS):
+    basepath = basepath[0 : -len(_TOOLS)]
+    sys.path.append(basepath)
+
+from pprint import pprint
+
+from queries.special import _SPECIAL_QUERIES
+
+_VERBS = {
+    "geturðu": "getur þú",
+    "viltu": "vilt þú",
+    "helduru": "heldur þú",
+    "kanntu": "kannt þú",
+    "ertu": "ert þú",
+    # "farðu": "far þú",
+    "þekkirðu": "þekkir þú",
+    "skilurðu": "skilur þú",
+    "talarðu": "talar þú",
+}
+
+
+def main() -> None:
+    special = _SPECIAL_QUERIES.keys()
+
+    for s in special:
+        for k, v in _VERBS.items():
+            if s.startswith(k):
+                i = s.replace(k, v)
+                if i not in special:
+                    print(s + " --> " + i)
+
+
+if __name__ == "__main__":
+    main()
