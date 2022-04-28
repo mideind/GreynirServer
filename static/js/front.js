0350df39b64519b70438bb6e2583b0330f0ecd5a
Sveinbjorn Thordarson <sveinbjorn@sveinbjorn.org>
2022-04-28 18:18:44 +0000

    Minor JS tweak

--- static/js/front.js
+++ static/js/front.js
@@ -441,7 +441,7 @@ function populateQueryResult(r) {
                });
             }
 
-      if (r.open_url !== null && r.open_url !== undefined) {
+      if (r.open_url) {
          window.open(r.open_url, '_blank');
       }
    }
