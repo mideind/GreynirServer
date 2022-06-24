43ba5616656bfe02100774346aa5330a0fb041a6
Logi E <logi.eyjolfsson@gmail.com>
2022-06-24 14:20:35 +0000

    Refactoring dialogue.py

--- queries/dialogue.py
+++ queries/dialogue.py
@@ -21,16 +21,10 @@ from queries import natlang_seq, sing_or_plur
 DIALOGUE_KEY = "dialogue"
 DIALOGUE_DATA_KEY = "dialogue_data"
 EMPTY_DIALOGUE_DATA = "{}"
+FINAL_RESOURCE_NAME = "Final"
 
 ResourceType = Union[str, int, float, bool, datetime.datetime, None]
 ListResourceType = List[ResourceType]
-
-
-class DialogueStructureType(TypedDict):
-    """ """
-
-    dialogue_name: str
-    resources: List["Resource"]
 
 
 class ResourceState(IntEnum):
@@ -53,7 +47,6 @@ class ResourceState(IntEnum):
 @dataclass
 class Resource:
     name: str = ""
-    required: bool = True
     data: Any = None
     state: ResourceState = ResourceState.INITIAL
     prompt: str = ""
@@ -62,17 +55,33 @@ class Resource:
     repeat_prompt: Optional[str] = None
     confirm_prompt: Optional[str] = None
     cancel_prompt: Optional[str] = None
+    requires: List[str] = field(default_factory=list)
     _repeat_count: int = 0
 
     def next_action(self) -> Any:
         raise NotImplementedError()
 
-    def generate_answer(self) -> str:
+    def generate_answer(self, dsm: "DialogueStateManager", result: Result) -> str:
         raise NotImplementedError()
 
     def update(self, new_data: Optional["Resource"]) -> None:
         if new_data:
             self.__dict__.update(new_data.__dict__)
+
+    def _execute_children(
+        self, dsm: "DialogueStateManager", result: Result
+    ) -> Optional[str]:
+        if "callbacks" in result:
+            while len(result.callbacks) > 0:
+                rnames, cb = result.callbacks.pop(0)
+                if self.name in rnames:
+                    cb(self, result)
+        if self.requires:
+            for rname in self.requires:
+                resource = dsm.get_resource(rname)
+                if resource.state is not ResourceState.CONFIRMED:
+                    return resource.generate_answer(dsm, result)
+        return None
 
 
 def _list_items(items: Any) -> str:
@@ -92,9 +101,12 @@ class ListResource(Resource):
     def list_available_options(self) -> str:
         raise NotImplementedError()
 
-    def generate_answer(self) -> str:
-        print("State: ", self.state)
-        ans: str = ""
+    def generate_answer(self, dsm: "DialogueStateManager", result: Result) -> str:
+        ans: Optional[str] = self._execute_children(dsm, result)
+        if ans:
+            return ans
+        if self.state is ResourceState.INITIAL:
+            self.state = ResourceState.UNFULFILLED
         if self.state is ResourceState.UNFULFILLED:
             if self._repeat_count == 0 or not self.repeatable:
                 ans = self.prompt
@@ -108,6 +120,8 @@ class ListResource(Resource):
                 ans = (
                     f"{self.confirm_prompt.format(list_items = _list_items(self.data))}"
                 )
+        if ans is None:
+            raise ValueError("No answer generated")
         return ans
 
 
@@ -123,14 +137,28 @@ class YesNoResource(Resource):
     yes_answer: Optional[str] = None
     no_answer: Optional[str] = None
 
-    def generate_answer(self) -> str:
-        ans = ""
+    def set_yes(self):
+        self.data = True
+        self.state = ResourceState.CONFIRMED
+
+    def set_no(self):
+        self.data = False
+        self.state = ResourceState.CONFIRMED
+
+    def generate_answer(self, dsm: "DialogueStateManager", result: Result) -> str:
+        ans: Optional[str] = self._execute_children(dsm, result)
+        if ans:
+            return ans
+        if self.state is ResourceState.INITIAL:
+            self.state = ResourceState.UNFULFILLED
         if self.data:
             if self.yes_answer:
                 ans = self.yes_answer
         else:
             if self.no_answer:
                 ans = self.no_answer
+        if ans is None:
+            raise ValueError("No answer generated")
         return ans
 
 
@@ -154,8 +182,12 @@ class DatetimeResource(Resource):
     def set_time(self, new_time: Optional[datetime.time] = None) -> None:
         self.data[1] = new_time
 
-    def generate_answer(self) -> str:
-        ans = ""
+    def generate_answer(self, dsm: "DialogueStateManager", result: Result) -> str:
+        ans: Optional[str] = self._execute_children(dsm, result)
+        if ans:
+            return ans
+        if self.state is ResourceState.INITIAL:
+            self.state = ResourceState.UNFULFILLED
         if self.state is ResourceState.UNFULFILLED:
             if self._repeat_count == 0 or not self.repeatable:
                 ans = self.prompt
@@ -185,6 +217,8 @@ class DatetimeResource(Resource):
                 )
         if self.state is ResourceState.CONFIRMED:
             ans = "Pöntunin er staðfest."
+        if ans is None:
+            raise ValueError("No answer generated")
         return ans
 
 
@@ -193,9 +227,35 @@ class NumberResource(Resource):
     data: int = 0
 
 
+@dataclass
+class OrResource(Resource):
+    data: Dict[str, Any] = field(default_factory=dict)
+    exclusive: bool = False  # Only one of the resources should be fulfilled
+
+
+@dataclass
+class FinalResource(Resource):
+    data: Any = None
+    final_prompt: str = ""
+
+    def generate_answer(self, dsm: "DialogueStateManager", result: Result) -> str:
+        ans: Optional[str] = self._execute_children(dsm, result)
+        if ans:
+            return ans
+        return self.final_prompt
+
+
 ##############################
 #    RESOURCE CLASSES END    #
 ##############################
+
+
+class DialogueStructureType(TypedDict):
+    """ """
+
+    dialogue_name: str
+    resources: List[Resource]
+
 
 _RESOURCE_TYPES: Mapping[str, Any] = {
     "Resource": Resource,
@@ -203,6 +263,7 @@ _RESOURCE_TYPES: Mapping[str, Any] = {
     "YesNoResource": YesNoResource,
     "DatetimeResource": DatetimeResource,
     "NumberResource": NumberResource,
+    "FinalResource": FinalResource,
 }
 
 
@@ -219,7 +280,6 @@ def _load_dialogue_structure(filename: str) -> DialogueStructureType:
         assert "name" in resource
         assert "type" in resource
         obj["resources"][i] = _RESOURCE_TYPES[resource["type"]](**resource)
-    print("OBJ:", obj)
     return cast(DialogueStructureType, obj)
 
 
@@ -228,7 +288,9 @@ class DialogueStateManager:
         self._dialogue_name: str = dialogue_name
         self._q: Query = query
         self._result: Result = result
+        self._resources: Dict[str, Resource] = {}
         self._saved_state: DialogueStructureType = self._get_saved_dialogue_state()
+        self._data: Dict[str, Any] = {}
         # TODO: CALL STACK!
         # TODO: Delegate answering from a resource to another resource or to another dialogue
         # TODO: í ávaxtasamtali "ég vil panta flug" "viltu að ég geymi ávaxtapöntunina eða eyði henni?" ...
@@ -244,12 +306,11 @@ class DialogueStateManager:
     def setup_dialogue(self):
         obj = _load_dialogue_structure(self._dialogue_name)
         print(obj)
-        self.resources: List[Resource] = []
         for i, resource in enumerate(obj["resources"]):
             if self._saved_state and i < len(self._saved_state["resources"]):
                 resource.update(self._saved_state["resources"][i])
                 resource.state = ResourceState(self._saved_state["resources"][i].state)
-            self.resources.append(resource)
+            self._resources[resource.name] = resource
 
         self.resourceState: Optional[Resource] = None
         self.ans: Optional[str] = None
@@ -270,37 +331,49 @@ class DialogueStateManager:
         self._set_dialogue_state(
             {
                 "dialogue_name": self._dialogue_name,
-                "resources": self.resources,
+                "resources": list(self._resources.values()),
             }
         )
 
+    def get_resource(self, name: str) -> Resource:
+        return self._resources[name]
+
     def generate_answer(self, result: Result) -> str:
-        i = 0
-        while i < len(self.resources):
-            resource = self.resources[i]
-            if resource.required and resource.state is not ResourceState.CONFIRMED:
-                if resource.state is ResourceState.INITIAL:
-                    resource.state = ResourceState.UNFULFILLED
-                if "callbacks" in result:
-                    while len(result.callbacks) > 0:
-                        r_name, cb = result.callbacks.pop(0)
-                        if self.resources[i].name in r_name:
-                            cb(resource, result)
-                        else:
-                            while i > -1:
-                                i -= 1
-                                if self.resources[i].name in r_name:
-                                    cb(self.resources[i], result)
-                                    break
-                    if (
-                        resource.state is ResourceState.CONFIRMED
-                        and resource != self.resources[-1]
-                    ):
-                        i += 1
-                        continue
-                return self.resources[i].generate_answer()
-            i += 1
-        return "Upp kom villa, reyndu aftur."
+        # if self._resources[FINAL_RESOURCE_NAME].state is not ResourceState.CONFIRMED:
+        ans = self._resources[FINAL_RESOURCE_NAME].generate_answer(self, result)
+        if self._resources[FINAL_RESOURCE_NAME].state is ResourceState.CONFIRMED:
+            # Final callback (performing some operation with the dialogue's data)
+            # should be called before ending dialogue
+            self.end_dialogue()
+        return ans
+
+        # i = 0
+        # while i < len(self.resources):
+        #     resource = self.resources[i]
+        #     if resource.required and resource.state is not ResourceState.CONFIRMED:
+        #         if resource.state is ResourceState.INITIAL:
+        #             resource.state = ResourceState.UNFULFILLED
+        #         if "callbacks" in result:
+        #             while len(result.callbacks) > 0:
+        #                 r_name, cb = result.callbacks.pop(0)
+        #                 if self.resources[i].name in r_name:
+        #                     cb(resource, result)
+        #                 else:
+        #                     while i > -1:
+        #                         i -= 1
+        #                         if self.resources[i].name in r_name:
+        #                             cb(self.resources[i], result)
+        #                             break
+        #             if (
+        #                 resource.state is ResourceState.CONFIRMED
+        #                 and resource != self.resources[-1]
+        #             ):
+        #                 self._data[resource.name] = resource.data
+        #                 i += 1
+        #                 continue
+        #         return self.resources[i].generate_answer()
+        #     i += 1
+        # return "Upp kom villa, reyndu aftur."
 
     def _get_saved_dialogue_state(self) -> DialogueStructureType:
         """Load the dialogue state for a client"""
