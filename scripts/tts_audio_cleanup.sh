cad0b50bb68b3fc08e5dba357fc5785b513d6152
Sveinbjorn Thordarson <sveinbjorn@sveinbjorn.org>
2023-01-04 23:32:56 +0000

    Updated docs

--- scripts/tts_audio_cleanup.sh
+++ scripts/tts_audio_cleanup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-# Delete all STT audio scratch files older than 24 hours
+# Delete all text-to-speech audio scratch files older than 1 day (24 hours)
 find /usr/share/nginx/greynir.is/static/audio/tmp/* -mtime +1 -exec rm {} \;
